23b7895c1f5a2bc3f96c98d958772fb47bde6386
FELIPE ALBINO RAFALDINI OLIVEIRA <00118107@unisoacad.uniso.br>
2025-03-11 10:09:15 -0300

    Exercicio 5

---
+++ 10-03-2025/5. Sanduiche.py
@@ -0,0 +1,7 @@
+sanduiche = int(input("Insira a quantidade de sanduiches: "))
+
+queijos = sanduiche * 2 * 50
+presuntos = sanduiche * 1 * 50
+hamburguers = sanduiche * 1 * 100
+
+print(f"Para fazer {sanduiche} será necessário:\n{queijos}g de queijo.\n{presuntos}g de presunto.\n{hamburguers}g de hamburguer.")
