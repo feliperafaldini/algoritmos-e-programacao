609798f57e02bb8d41f8d8234b6d6910187f1176
feliperafaldini <feliperafaldini@gmail.com>
2025-03-23 16:25:31 -0300

    Descrição narrativa adicionada

---
+++ 10-03-2025/5. Sanduiche.py
@@ -0,0 +1,9 @@
+sanduiches = int(input("Insira a quantidade de sanduíches: "))
+
+queijos = sanduiches * 2 * 50
+presuntos = sanduiches * 1 * 50
+hamburguers = sanduiches * 1 * 500
+
+print(
+    f"Será necessário a compra de:\n{queijos} gramas de queijo.\n{presuntos} gramas de presunto.\n{hamburguers} gramas de hamburguer."
+)
